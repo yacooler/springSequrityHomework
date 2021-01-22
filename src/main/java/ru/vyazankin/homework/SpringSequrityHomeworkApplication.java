72d597639406152fff3a7967e13edad1806768ca
yacooler <yacooler@yandex.ru>
2021-01-23 02:31:59 +0300

    Initial commit

---
+++ src/main/java/ru/vyazankin/homework/SpringSequrityHomeworkApplication.java
@@ -0,0 +1,13 @@
+package ru.vyazankin.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSequrityHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSequrityHomeworkApplication.class, args);
+	}
+
+}
